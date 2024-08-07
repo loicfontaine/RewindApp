f114406efc6278daf8019fcb0f071723e0a9e400
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2024-08-07 18:32:54 +0200

    Remove nuxconfig from gitignore

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: false },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
+  ssr: false,
+  //ESSAI
+
+
+
+  /*
+  typescript //remove at end bcz slow down
+    : {
+    typeCheck
+      : true
+  },
+  */
+  runtimeConfig: {
+    public: {
+      googleClientId: process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID,
+      googleApiKey: process.env.NUXT_PUBLIC_GOOGLE_API_KEY,
+      googleDiscoveryDoc: "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+      googleScopes: "https://www.googleapis.com/auth/calendar.readonly",
+    }
+
+  },
+  vite: {
+    build: {
+      target: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15'],
+    },
+  },
+  nitro: {
+    esbuild: {
+      options: {
+        target: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15'],
+      }
+
+    }
+  }
+
+
+
+
+});
